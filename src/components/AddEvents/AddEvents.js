70179c659531e25d5cc7f4f70f31b73d340354a0
Mahfuz Islam <rjmahfuztech@gmail.com>
2021-03-31 00:15:27 +0600

    added link

--- src/components/AddEvents/AddEvents.js
+++ src/components/AddEvents/AddEvents.js
@@ -12,7 +12,7 @@ const AddEvents = () => {
       imageURL: imageURL
     }
 
-    const url = `http://localhost:4500/addEvent`;
+    const url = `https://rocky-fortress-55251.herokuapp.com/addEvent`;
     fetch(url, {
       method: 'POST',
       headers: {
